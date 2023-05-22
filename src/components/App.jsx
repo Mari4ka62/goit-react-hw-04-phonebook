094dce9d59dcca23b29bbac378cfcdca3088600f
Maria Vovchuk <mari4ka62@gmail.com>
2023-05-22 11:39:42 +0300

    change to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,59 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-    isAdd: false,
-    isDelete: false,
+export function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (parsedContacts) {
+      setContacts(parsedContacts);
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+  const findContacts = e => {
+    setFilter(e.currentTarget.value);
   };
-  componentDidMount() {
-    const localData = localStorage.getItem('contact');
-    if (localData) {
-      this.setState({ contacts: JSON.parse(localData) });
-    }
-  }
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
-    }
-    if (prevState.contacts.length < this.state.contacts.length) {
-      this.setState({ isAdd: true });
-      setTimeout(() => {
-        this.setState({ isAdd: false });
-      }, 1500);
-    }
-    if (prevState.contacts.length > this.state.contacts.length) {
-      this.setState({ isDelete: true });
-      setTimeout(() => {
-        this.setState({ isDelete: false });
-      }, 1500);
-    }
-  }
-  findContacts = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-  addContacts = ({ name, number }) => {
+  const addContacts = (name, number) => {
     const contact = {
       name,
       number,
       id: nanoid(3),
     };
-    const duplicatedContact = this.state.contacts.find(
+    const duplicatedContact = contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
-
     if (duplicatedContact) {
       return alert(`${duplicatedContact.name} is already in contacts`);
     }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
+    setContacts(state => [...state, contact]);
   };
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
-  render() {
-    const actualContact = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
-    );
-    return (
-      <div className="container">
-        <h2 className="title">Phonebook</h2>
-        <ContactForm onSubmit={this.addContacts} />
-        {this.state.isAdd && <div role="alert">Add number successfully!</div>}
-        {this.state.isDelete && (
-          <div role="alert">Deleted number successfully!</div>
-        )}
+  const addActualContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
 
-        <Filter filterData={this.state.filter} onChange={this.findContacts} />
-        <h2 className="title">Contacts</h2>
-        <ContactList
-          contacts={actualContact}
-          deleteContact={this.deleteContact}
-        />
-      </div>
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
-  }
+  };
+  return (
+    <div className="container">
+      <h2 className="title">Phonebook</h2>
+      <ContactForm onSubmit={addContacts} />
+      <Filter filterData={filter} onChange={findContacts} />
+      <h2 className="title">Contacts</h2>
+      <ContactList
+        contacts={addActualContacts()}
+        deleteContact={deleteContact}
+      />
+    </div>
+  );
 }
